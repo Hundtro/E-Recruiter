d245920c8c3d59de70e58f3241d6cd905b726e5b
Hundtro <dhund@protonmail.com>
2020-08-05 06:20:31 +0200

    change database structure

--- Data/create.sql
+++ Data/create.sql
@@ -1,13 +1,30 @@
---run in database data.db with sqlite
 --create tables
-CREATE TABLE "Reminder" (
-	"Id"	  INTEGER PRIMARY KEY AUTOINCREMENT,
-	"Value"	  TEXT NOT NULL,
-	"Created" TEXT
+
+CREATE TABLE "Users" (
+    "Id"        INTEGER PRIMARY KEY AUTOINCREMENT,    
+    "Login"     TEXT NOT NULL,
+    "Password"  TEXT NOT NULL,
+    "FullName"  TEXT NOT NULL,
+    "CanEdit"   INTEGER DEFAULT 0,
+    "CanConfig" INTEGER DEFAULT 0
 );
 
-CREATE TABLE Employee (
-	"Id"           INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE "Reminders" (
+    "Id"        INTEGER PRIMARY KEY AUTOINCREMENT,
+    "Text"      TEXT NOT NULL,
+    "Created"   TEXT NOT NULL,
+    "CreatedBy" INTEGER NOT NULL,
+    FOREIGN KEY(CreatedBy) REFERENCES Users(Id)
+);
+
+CREATE TABLE "EmailTemplates" (
+    "Id"      INTEGER PRIMARY KEY AUTOINCREMENT,
+    "Title"   TEXT NOT NULL,
+    "Message" TEXT NOT NULL
+);
+
+CREATE TABLE Candidates (
+    "Id"           INTEGER PRIMARY KEY AUTOINCREMENT,
     "Name"         TEXT NOT NULL,
     "Surname"      TEXT NOT NULL,
 	"BirthDate"    TEXT NOT NULL,
@@ -16,47 +33,55 @@ CREATE TABLE Employee (
 	"Email"        TEXT,
 	"MobilePhone"  TEXT,
 	"HomeOffice"   INTEGER NOT NULL,
-	"ExWorker"     INTEGER NOT NULL
+	"ExWorker"     INTEGER NOT NULL,
+    "Photo"        BLOB,
+    "CVFile"       BLOB,
+    "CreatedBy"    INTEGER NOT NULL,
+    FOREIGN KEY(CreatedBy) REFERENCES Users(Id)
 );
 
-CREATE TABLE JobTitle (
-	"Id"	        INTEGER PRIMARY KEY AUTOINCREMENT,
-	"Title"         TEXT NOT NULL,
-	"DefaultSalary" TEXT NOT NULL,
-	"Description"   TEXT
+CREATE TABLE JobTitles (
+    "Id"	        INTEGER PRIMARY KEY AUTOINCREMENT,
+    "Title"         TEXT NOT NULL,
+    "DefaultSalary" TEXT NOT NULL,
+    "Description"   TEXT,
+    "CreatedBy"     INTEGER NOT NULL,
+    FOREIGN KEY(CreatedBy) REFERENCES Users(Id)
 );
 
 CREATE TABLE Experience (
-	"Id"			INTEGER PRIMARY KEY AUTOINCREMENT,
-	"EmployeeId"    INTEGER NOT NULL,
-	"Name"          TEXT NOT NULL,
-	"Title"         TEXT NOT NULL,
-	"StartDate"     TEXT NOT NULL,
-	"EndDate"       TEXT NOT NULL,
-	FOREIGN KEY(EmployeeId) REFERENCES Employee(Id)
+    "Id"            INTEGER PRIMARY KEY AUTOINCREMENT,
+    "CandidateId"   INTEGER NOT NULL,
+    "Name"          TEXT NOT NULL,
+    "Title"         TEXT NOT NULL,
+    "StartDate"     TEXT NOT NULL,
+    "EndDate"       TEXT NOT NULL,
+	FOREIGN KEY(CandidateId) REFERENCES Candidates(Id)
 );
 
-CREATE TABLE HireStep (
-	"Id"			INTEGER PRIMARY KEY AUTOINCREMENT,
-	"JobTitleId"    INTEGER NOT NULL,
-	"Order"         INTEGER,
-	"Name"          TEXT NOT NULL,
-	"Description"   TEXT,
-	FOREIGN KEY(JobTitleId) REFERENCES JobTitle(Id)
+CREATE TABLE HireSteps (
+    "Id"			INTEGER PRIMARY KEY AUTOINCREMENT,
+    "JobTitleId"    INTEGER NOT NULL,
+    "Order"         INTEGER,
+    "Name"          TEXT NOT NULL,
+    "Description"   TEXT,
+    FOREIGN KEY(JobTitleId) REFERENCES JobTitles(Id)
 );
 
 CREATE TABLE HireProcess (
-	"Id" 			INTEGER PRIMARY KEY AUTOINCREMENT,
-	"EmployeeId"    INTEGER NOT NULL,
-	"JobTitleId"    INTEGER NOT NULL,
-	"StepId"        INTEGER NOT NULL,
-	"Comments"      TEXT,
-	FOREIGN KEY(EmployeeId) REFERENCES Employee(Id),
-	FOREIGN KEY(JobTitleId) REFERENCES JobTitle(Id),
-	FOREIGN KEY(StepId) REFERENCES HireStep(Id)
+    "Id"          INTEGER PRIMARY KEY AUTOINCREMENT,
+    "CandidateId" INTEGER NOT NULL,
+    "JobTitleId"  INTEGER NOT NULL,
+    "StepId"      INTEGER NOT NULL,
+    "Comments"    TEXT,
+    FOREIGN KEY(CandidateId) REFERENCES Candidates(Id),
+    FOREIGN KEY(JobTitleId) REFERENCES JobTitles(Id),
+    FOREIGN KEY(StepId) REFERENCES HireSteps(Id)
 );
 
 --Create default values
-INSERT INTO main.JobTitle ("Title", "DefaultSalary") VALUES ('General', 0);
-INSERT INTO main.HireStep ("JobTitleId", "Order", "Name") VALUES ((SELECT Id FROM main.JobTitle WHERE Title = 'General'), 1, 'Default');
+
+INSERT INTO main.Users ("Login", "Password", "FullName", "CanEdit", "CanConfig") VALUES ("admin", "admin", "admin", 1, 1);  
+INSERT INTO main.JobTitles ("Title", "DefaultSalary", "CreatedBy") VALUES ('General', 0, (SELECT Id FROM main.Users WHERE Login ='admin'));
+INSERT INTO main.HireSteps ("JobTitleId", "Order", "Name") VALUES ((SELECT Id FROM main.JobTitles WHERE Title = 'General'), 1, 'Default');
 
